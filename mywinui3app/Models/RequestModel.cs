f90cb2c61ecb9b8313602f5ea67dff480d7afe05
Carlos Marin <camarin@microsoft.com>
2024-07-02 13:20:26 -0400

    Added initial Models and ViewModels for the Request implementation

---
+++ mywinui3app/Models/RequestModel.cs
@@ -0,0 +1,45 @@
+﻿namespace mywinui3app.Models;
+public class RequestModel
+{
+    public string RequestId { get; set; }
+    public string Name { get; set; }
+    public string Method { get; set; }
+    public URL URL { get; set; }
+    public Parameters Parameters { get; set; }
+    public Parameters Headers { get; set; }
+    public Body Body { get; set; }
+
+}
+
+public class URL
+{
+    public string RawURL { get; set; }
+    public string Protocol { get; set; }
+    public ICollection<string> Host { get; set; }
+    public ICollection<string> Path { get; set; }
+    public IDictionary<string, string> Variables { get; set; }
+ }
+
+public class Parameters
+{
+    public ICollection<FormData> Items { get; set; }
+}
+
+public class Headers
+{
+    public ICollection<FormData> Items { get; set; }
+}
+
+
+public class Body
+{
+    public string Mode { get; set; }
+    public ICollection<FormData> Items { get; set; }
+}
+
+public class FormData
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+    public string Description { get; set; }
+}
